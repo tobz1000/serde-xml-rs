9dbf1e66ffcb47e5bb057fb731107dbc5e2a348c
Toby Dimmick <Toby.Dimmick@bistech.co.uk>
2020-09-30 21:15:58 +0100

    Add failing test

--- tests/test.rs
+++ tests/test.rs
@@ -159,3 +159,60 @@ fn collection_of_enums() {
         }
     );
 }
+
+#[test]
+fn out_of_order_collection() {
+    #[derive(Debug, Deserialize, PartialEq)]
+    struct Collection {
+        a: Vec<A>,
+        b: Vec<B>,
+        c: C
+    }
+
+    #[derive(Debug, Deserialize, PartialEq)]
+    struct A {
+        name: String
+    }
+
+    #[derive(Debug, Deserialize, PartialEq)]
+    struct B {
+        name: String
+    }
+
+    #[derive(Debug, Deserialize, PartialEq)]
+    struct C {
+        name: String
+    }
+
+    let _ = simple_logger::init();
+
+    let in_xml = r#"
+        <collection>
+            <a name="a1" />
+            <a name="a2" />
+            <b name="b1" />
+            <a name="a3" />
+            <c name="c" />
+            <b name="b2" />
+            <a name="a4" />
+        </collection>
+    "#;
+
+    let should_be = Collection {
+        a: vec![
+            A { name: "a1".into() },
+            A { name: "a2".into() },
+            A { name: "a3".into() },
+            A { name: "a4".into() },
+        ],
+        b: vec![
+            B { name: "b1".into() },
+            B { name: "b2".into() },
+        ],
+        c: C { name: "c".into() }
+    };
+
+    let actual: Collection = from_str(&in_xml).unwrap();
+
+    assert_eq!(should_be, actual);
+}
